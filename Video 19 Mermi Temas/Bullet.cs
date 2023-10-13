b2bf68fd7a2773aa17e9994cddc27c1a9b9d2439
Ahmet Emre YILDIZ <ahmetemreyildiz51@gmail.com>
2023-10-13 11:31:33 +0300

    Video 19 Eklendi

---
+++ Video 19 Mermi Temas/Bullet.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float lifetime;
+
+    private void Awake()
+    {
+        Destroy(this.gameObject, lifetime);
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * speed *Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Enemy"))
+        {
+            print("Dusmana Temas Ettim");
+            Destroy(this.gameObject);
+        }
+    }
+}
