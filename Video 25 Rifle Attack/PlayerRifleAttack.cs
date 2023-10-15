6caa1192712db760bb9553b43e132c227fd1aadf
Ahmet Emre YILDIZ <ahmetemreyildiz51@gmail.com>
2023-10-15 18:05:51 +0300

    Video 25 Eklendi

---
+++ Video 25 Rifle Attack/PlayerRifleAttack.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRifleAttack : MonoBehaviour
+{
+    // Bu Script Rifle Icin
+
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private Transform bulletSpawnPoint;
+
+    [SerializeField] private float targetTime = 1;
+    [SerializeField] private float bulletSpawnTimer = 1;
+
+    private bool isShooting;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            isShooting = true;
+
+        if (Input.GetMouseButtonUp(0))
+            isShooting = false;
+
+        bulletSpawnTimer -= Time.deltaTime;
+
+        if (isShooting && bulletSpawnTimer < 0)
+        {
+            Instantiate(bullet, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+
+            bulletSpawnTimer = targetTime;
+        }
+    }
+}
+
